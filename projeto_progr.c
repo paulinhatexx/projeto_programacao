87d841462b9b3cd5095f9a6374811133816b5f83
paulinhatexx <paulinha.texx@hotmail.com>
2020-09-29 18:14:57 +0000

    tela sobre

--- projeto_progr.c
+++ projeto_progr.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
-int main(){
-    printf("Projeto Programação\n");
-    printf("Ana Paula\n");
+void menuSobre(void);
+int main (void){
+    menuSobre();
     return 0;
+}
+
+void menuSobre(void){
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                   ///\n");
+    printf("///            Universidade Federal do Rio Grande do Norte            ///\n");
+    printf("///               Centro de Ensino Superior do Seridó                 ///\n");
+    printf("///               Departamento de Computação e Tecnologia             ///\n");
+    printf("///                 Disciplina DCT1106 -- Programação                 ///\n");
+    printf("///           Projeto Sistema de Gestão de Finanças Domésticas        ///\n");
+    printf("///                 Developed by @paulinhatexx - Sep,2020             ///\n");
+    printf("///                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                   ///\n");
+    printf("///         === Sistema de Gestão de Finanças Domésticas ===          ///\n");   
+    printf("///                                                                   ///\n");
+    printf("///   Programa a ser desenvolvido na discipina Programação, tendo     ///\n");
+    printf("///  como objetivo o controle de finanças doméstica, buscando o       ///\n");
+    printf("///  equilíbrio do orçamento familiar.O programa deve realizar o      ///\n");
+    printf("///  monitoramento do fluxo financeiro de uma residência através do   ///\n");
+    printf("///  registro de receitas e despesas, podendo conter outras eventuais ///\n");
+    printf("///  funcionalidades.                                                 ///\n");
+    printf("///                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
 }
