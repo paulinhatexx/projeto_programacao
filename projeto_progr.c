10f7dd979633c46c6d8c74652e776bb971a6b452
paulinhatexx <paulinha.texx@hotmail.com>
2020-10-05 18:51:32 +0000

    tela menu

--- projeto_progr.c
+++ projeto_progr.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+
 void menuSobre(void);
+void menuReceita(void);
+void menuPagamentos(void);
+void menuDespesa(void);
+
+
 int main (void){
     menuSobre();
+getchar();
+    
     return 0;
 }
+
+
 
 void menuSobre(void){
     printf("\n");
@@ -18,18 +28,80 @@ void menuSobre(void){
     printf("///                                                                   ///\n");
     printf("/////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                   ///\n");
-    printf("///         === Sistema de Gestão de Finanças Domésticas ===          ///\n");   
+    printf("///         === Sistema de Gestão de Finanças Domésticas ===          ///\n"); 
+    printf("///                                                                   ///\n");  
+    printf("///                      === SIG-Finance ===                          ///\n");
     printf("///                                                                   ///\n");
     printf("///   Programa a ser desenvolvido na discipina Programação, tendo     ///\n");
     printf("///  como objetivo o controle de finanças doméstica, buscando o       ///\n");
     printf("///  equilíbrio do orçamento familiar.O programa deve realizar o      ///\n");
     printf("///  monitoramento do fluxo financeiro de uma residência através do   ///\n");
-    printf("///  registro de receitas e despesas, podendo conter outras eventuais ///\n");
-    printf("///  funcionalidades.                                                 ///\n");
+    printf("///  registro de receitas, despesas, compras e pagamentos,podendo     ///\n");
+    printf("///  conter outras eventuais funcionalidades.                         ///\n");
     printf("///                                                                   ///\n");
     printf("/////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
-    printf("Menu\n");
-    printf("Escolha o número da opção desejada: \n");
-    
+}
+
+void menuReceita(void) {
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                  |///\n");
+    printf("///                     *** SIG-Finance ***                          |///\n");
+    printf("///                                                                  |///\n");
+    printf("///__________________________________________________________________|///\n");
+    printf("///                      ** Menu Receita **                           ///\n");
+    printf("///                                                                   ///\n");
+    printf("/// Escolha a opção desejada:                                         ///\n");
+    printf("///                                                                   ///\n");
+    printf("/// 1. Cadastrar receita                                              ///\n");
+    printf("/// 2. Atualizar receita                                              ///\n");
+    printf("/// 3. Excluir Receita                                                ///\n");
+    printf("/// 4. Voltar ao Menu Anterior                                        ///\n");
+    printf("///                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+         
+}
+
+void menuPagamentos(void) {
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                  |///\n");
+    printf("///                     *** SIG-Finance ***                          |///\n");
+    printf("///                                                                  |///\n");
+    printf("///__________________________________________________________________|///\n");
+    printf("///                      ** Menu Pagamentos **                        ///\n");
+    printf("///                                                                   ///\n");
+    printf("/// Escolha a opção desejada:                                         ///\n");
+    printf("///                                                                   ///\n");
+    printf("/// 1. Cadastrar Pagamento                                            ///\n");
+    printf("/// 2. Atualizar Pagamento                                            ///\n");
+    printf("/// 3. Excluir Pagamento                                              ///\n");
+    printf("/// 4. Voltar ao Menu Anterior                                        ///\n");
+    printf("///                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+         
+}
+
+void menuDespesa(void) {
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                  |///\n");
+    printf("///                     *** SIG-Finance ***                          |///\n");
+    printf("///                                                                  |///\n");
+    printf("///__________________________________________________________________|///\n");
+    printf("///                     ** Menu Despesas **                           ///\n");
+    printf("///                                                                   ///\n");
+    printf("/// Escolha a opção desejada:                                         ///\n");
+    printf("///                                                                   ///\n");
+    printf("/// 1. Cadastrar Despesa                                              ///\n");
+    printf("/// 2. Atualizar Despesa                                              ///\n");
+    printf("/// 3. Excluir Despesa                                                ///\n");
+    printf("/// 4. Voltar ao Menu Anterior                                        ///\n");
+    printf("///                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+         
 }
