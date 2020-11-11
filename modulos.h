2e8c64d27b1d14402bca1cb58a456161ec0dc9ba
Ana Paula <71039892+paulinhatexx@users.noreply.github.com>
2020-11-11 14:38:27 -0300

    Update modulos.h

--- modulos.h
+++ modulos.h
@@ -1,1 +1,25 @@
+void moduloReceitas(void);
+void moduloPagamentos(void);
+void moduloDespesas(void);
+void moduloProduto(void);
+void moduloConsultas(void);
 
+void cadastrarReceita(void);
+void atualizarReceita(void);
+void excluirReceita(void);
+
+void cadastrarPagamento(void);
+void atualizarPagamento(void);
+void excluirPagamento(void);
+
+void cadastrarDespesa(void);
+void atualizarDespesa(void);
+void excluirDespesa(void);
+
+void incluirProduto(void);
+void alterarProduto(void);
+void excluirProduto(void);
+
+void consultarOrcamento(void);
+void consultarDespesas(void);
+void consultarPagamentos(void);
