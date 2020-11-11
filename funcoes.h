c1276359688f49d6d87610c9ccf6cb18a170209b
Ana Paula <71039892+paulinhatexx@users.noreply.github.com>
2020-11-11 13:57:02 -0300

    Delete funcoes.h

--- funcoes.h
+++
@@ -1,9 +0,0 @@
-char telaPrincipal(void);
-char menuReceita(void);
-char menuPagamentos(void);
-char menuDespesa(void);
-char menuProduto(void);
-char menuConsulta(void);
-void sobre(void);
-int dataValida(int);
-int bissexto(int);
