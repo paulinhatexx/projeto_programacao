7f50bb16f04e6fc8c6167b243561aefc166b9eac
Ana Paula <71039892+paulinhatexx@users.noreply.github.com>
2020-12-02 16:59:32 -0300

    Update leituras.c
    função leituras

--- leituras.c
+++ leituras.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "validacoes.h"
+
+char* leitura(void);
+
+char* lerReceita(void){
+    char *receita;
+    receita= leitura();
+    while(!ehLetra(receita)){
+        printf("Dados Inválidos!!");
+        printf("Tente Novamente!");
+        receita= leitura();
+    }
+    return receita;
+
+}
+
+
+
+
+char* leitura(void) {
+	int tam;
+	char dadoLido[256];
+	char *dadoSalvo;
+	scanf(" %255[^\n]", dadoLido);
+	tam = strlen(dadoLido) + 1;
+	dadoSalvo = (char*) malloc(tam * sizeof(char));
+	strcpy(dadoSalvo, dadoLido);
+	return dadoSalvo;
+}
 
