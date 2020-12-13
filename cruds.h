a051a68fefc76452679b0777e8ef2ef009fb2644
Ana Paula <71039892+paulinhatexx@users.noreply.github.com>
2020-12-12 21:53:52 -0300

    Update cruds.h

--- cruds.h
+++ cruds.h
@@ -2,3 +2,5 @@ void cadastrarReceita(void);
 void cadastrarDespesa(void);
 void cadastrarPagamento(void);
 void incluirProduto(void);
+void exibeReceita (Receita* rec);
+void gravaReceita (Receita* rec);
