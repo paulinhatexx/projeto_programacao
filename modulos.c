7843be271c6044f00fe6d4255bcf2313822319fa
Ana Paula <71039892+paulinhatexx@users.noreply.github.com>
2020-12-13 22:20:38 -0300

    Update modulos.c

--- modulos.c
+++ modulos.c
@@ -2,26 +2,6 @@
 #include"menus.h"
 #include"cruds.h"
 
-void cadastrarReceita(void);
-void atualizarReceita(void);
-void excluirReceita(void);
-
-void cadastrarPagamento(void);
-void atualizarPagamento(void);
-void excluirPagamento(void);
-
-void cadastrarDespesa(void);
-void atualizarDespesa(void);
-void excluirDespesa(void);
-
-void incluirProduto(void);
-void alterarProduto(void);
-void excluirProduto(void);
-
-void consultarOrcamento(void);
-void consultarDespesas(void);
-void consultarPagamentos(void);
-
 
 void moduloReceitas(void){
     char opcao;
