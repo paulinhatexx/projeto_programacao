88f4fae9e96cf5ff9f6326f0f2583d5d96e43cd8
Ana Paula <71039892+paulinhatexx@users.noreply.github.com>
2020-12-17 21:30:08 -0300

    Update cruds.h

--- cruds.h
+++ cruds.h
@@ -1,4 +1,38 @@
+typedef struct receita Receita;
+typedef struct despesa Despesa;
+typedef struct pagamento Pagamento;
+typedef struct produto Produto;
+
+struct receita{
+   char origem[26];
+   char valor[9];
+   char data[11];
+   char status;
+};
+
+struct despesa{
+   char tipo[26];
+   char valor[9];
+   char data[11];
+   char status;
+};
+
+struct pagamento{
+   char destino[31];
+   char valor[9];
+   char data[11];
+   char status;
+};
+
+struct produto{
+   char tipo[26];
+   char valor[9];
+   char data[11];
+   char status;
+};
+
 void cadastrarReceita(void);
+void atualizarReceita(void);
 void alterarDespesa(void);
 void excluirReceita(void);
 void cadastrarDespesa(void);
@@ -14,11 +48,11 @@ void consultarOrcamento(void);
 void consultarDespesas(void);
 void consultarPagamentos(void);
 
-void exibeReceita (void);
-void gravaReceita (void);
-void exibePagamento(void);
-void gravaPagamento(void);
-void exibeDespesa (void);
-void gravaDespesa (void);
-void exibeProduto (void);
-void gravaProduto (void);
+void exibeReceita (Receita*);
+void gravaReceita (Receita*);
+void exibePagamento(Pagamento*);
+void gravaPagamento(Pagamento*);
+void exibeDespesa (Despesa*);
+void gravaDespesa (Despesa*);
+void exibeProduto (Produto*);
+void gravaProduto (Produto*);
