2e300cee4e4c560394f95c4d1369c6f1ffe90d0f
Ana Paula <71039892+paulinhatexx@users.noreply.github.com>
2020-12-18 17:11:10 -0300

    Update cruds.c

--- cruds.c
+++ cruds.c
@@ -91,6 +91,7 @@ void atualizarReceita(void){
     printf("///                                                                   ///\n");
     printf("///  Digite a receita a ser alterada:                                 ///\n");
     scanf(" %25[^\n]", alterar);
+    getchar();
     rec = (Receita*) malloc(sizeof(Receita));
     while((!achou)&&(fread(rec,sizeof(Receita), 1,fp))){
        if((strcmp(rec->origem, alterar) == 0) && (rec->status == '1')){
@@ -127,16 +128,62 @@ void atualizarReceita(void){
 }
 
 void excluirReceita(void){
-    printf("\n///  **    Excluir Receita   **   ///\n\n");
-    printf("Em desenvolvimento...\n\n");
-    printf("Tecle ENTER pra continuar..");
+    FILE* fp;
+    Receita* rec;
+    int achou;
+    char resp;
+    char alterar[26];
+    fp = fopen("receita.dat", "r+b");
+    if (fp == NULL) {
+        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+       printf("Não é possível continuar o programa...\n");
+        exit(1);
+    }
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("///|                                                                 |///\n");
+    printf("///|                    *** SIG-Finance ***                          |///\n");
+    printf("///|                                                                 |///\n");
+    printf("///|_________________________________________________________________|///\n");
+    printf("///                    **   Excluir  Receita **                       ///\n");
+    printf("///                                                                   ///\n");
+    printf("///                                                                   ///\n");
+    printf("///  Digite a receita a ser excluida:                                 ///\n");
+    scanf(" %14[^\n]",alterar);
+    rec = (Receita*) malloc(sizeof(Receita));
+    achou = 0;
+    while((!achou)&&(fread(rec,sizeof(Receita), 1,fp))){
+       if((strcmp(rec->origem, alterar) == 0) && (rec->status == '1')){
+         achou = 1;
+       }
+    }
+  
+    if (achou) {
+        exibeReceita(rec);
+        getchar();
+        printf("Deseja realmente apagar esta receita (s/n)? ");
+        scanf("%c", &resp);
+        if (resp == 's' || resp == 'S') {
+            rec->status = '0';
+            fseek(fp, (-1)*sizeof(Receita), SEEK_CUR);
+            fwrite(rec, sizeof(Receita), 1, fp);
+            printf("\nReceita excluída com sucesso!!!\n");
+        } else {
+            printf("\n Os dados não foram alterados\n");
+        }
+     } else {
+         printf("Esta receita %s não foi encontrada..\n", alterar);
+    }
+    free(rec);
+    fclose(fp);
+}
     getchar();
 }
 
 void cadastrarPagamento(void){
     Pagamento* pag;
     pag = (Pagamento*) malloc(sizeof(Pagamento));
-   FILE* fp;
+    FILE* fp;
    fp = fopen("pagamento.dat" , "ab");
    if (fp == NULL){
       printf("Ocorreu um erro na abertura do arquivo!!");
@@ -176,19 +223,122 @@ void exibePagamento (Pagamento* pag){
    printf("Destino do Pagamento: %s\n" , pag->destino);
    printf("Valor do Pagamento: %s\n" , pag->valor);
    printf("Vencimento do Pagamento: %s\n" , pag->data);
-   printf("Status: %c\n" , pag->status);
 }
+
 void atualizarPagamento(void){
-    printf("\n///   **   Atualizar Pagamento  **///\n\n");
-    printf(" Em desenvolvimento...\n\n");
+    FILE* fp;
+    int achou;
+    char resp;
+    char alterar[26];
+    fp = fopen("pagamento.dat" , "r+b");
+    if (fp == NULL){
+      printf("Ocorreu um erro na abertura do arquivo!!");
+      exit(1);
+   }
+    Pagamento* pag;
+    achou = 0;
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("///|                                                                 |///\n");
+    printf("///|                    *** SIG-Finance ***                          |///\n");
+    printf("///|                                                                 |///\n");
+    printf("///|_________________________________________________________________|///\n");
+    printf("///                    **   Atualizar  Pagamento **                   ///\n");
+    printf("///                                                                   ///\n");
+    printf("///                                                                   ///\n");
+    printf("///  Digite o pagamento a ser alterada:                               ///\n");
+    scanf(" %25[^\n]", alterar);
+    getchar();
+    pag = (Pagamento*) malloc(sizeof(Pagamento));
+    while((!achou)&&(fread(pag,sizeof(Pagamento), 1,fp))){
+       if((strcmp(pag->destino, alterar) == 0) && (pag->status == '1')){
+           achou = 1;
+       }
+   }
+    if (achou){
+     exibePagamento(rec);
+     printf("Deseja alterar  este  pagamento?(s/n)");
+     scanf("%c",&resp);
+      if (resp =='s' || resp == 'S'){
+       printf("///  Digite o destino do pagamento:                                  ///\n");
+       scanf(" %25[^\n]", pag->destino);
+       printf("///Digite o valor do pagamento:                                      /// \n");
+       scanf(" %8[^\n]", pag->valor);
+       printf("/// Digite a data de vencimento (dd/mm/aaaa):                        /// \n");
+       scanf(" %10[^\n]", pag->data);
+       pag->status = '1';
+       fseek(fp, (-1)*sizeof(Pagamento), SEEK_CUR);
+       fwrite(rec, sizeof(Pagamento), 1, fp);
+       printf("\nPagamento  atualizado!!!\n");
+      }else {
+       printf("\n Os dados não serão alterados\n");
+      }
+    } else {
+     printf("Este pagamento %s não foi encontrado !!\n", alterar);
+    }
+    free(pag);
+    fclose(fp);
+    printf("///                                                                   ///\n");
+    printf("///                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
     printf("Tecle ENTER pra continuar..");
     getchar();
-
 }
 
 void excluirPagamento(void){
-    printf("\n///   **   Excluir Pagamento  **   ///\n\n");
-    printf(" Em desenvolvimento...\n\n");
+    FILE* fp;
+    Pagamento* pag;
+    int achou;
+    char resp;
+    char alterar[26];
+    fp = fopen("pagamento.dat", "r+b");
+    if (fp == NULL) {
+        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+       printf("Não é possível continuar o programa...\n");
+        exit(1);
+    }
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("///|                                                                 |///\n");
+    printf("///|                    *** SIG-Finance ***                          |///\n");
+    printf("///|                                                                 |///\n");
+    printf("///|_________________________________________________________________|///\n");
+    printf("///                    **   Excluir  Pagamento **                     ///\n");
+    printf("///                                                                   ///\n");
+    printf("///                                                                   ///\n");
+    printf("///  Digite o pagamento a ser excluido:                               ///\n");
+    scanf(" %14[^\n]",alterar);
+    pag = (Pagamento*) malloc(sizeof(pagamento));
+    achou = 0;
+    while((!achou)&&(fread(pag,sizeof(Pagamento), 1,fp))){
+       if((strcmp(pag->destino, alterar) == 0) && (pag->status == '1')){
+         achou = 1;
+       }
+    }
+  
+    if (achou) {
+        exibePagamento(pag);
+        getchar();
+        printf("Deseja realmente apagar este pagamento (s/n)? ");
+        scanf("%c", &resp);
+        if (resp == 's' || resp == 'S') {
+            pag->status = '0';
+            fseek(fp, (-1)*sizeof(Pagamento), SEEK_CUR);
+            fwrite(pag, sizeof(Pagamento), 1, fp);
+            printf("\nPagamento excluído com sucesso!!!\n");
+        } else {
+            printf("\n Os dados não foram alterados\n");
+        }
+     } else {
+         printf("Este pagamento%s não foi encontrado..\n", alterar);
+    }
+    free(pag);
+    fclose(fp);
+}
+    getchar();
+}
+
     printf("Tecle ENTER pra continuar..");
     getchar();
 }
@@ -251,14 +401,61 @@ void atualizarDespesa(void){
 }
 
 void excluirDespesa(void){
-    printf("\n///   **   Excluir Despesa  **   ///\n\n");
-    printf(" Em desenvolvimento...\n\n");
+    FILE* fp;
+    Despesa* des;
+    int achou;
+    char resp;
+    char alterar[26];
+    fp = fopen("despesa.dat", "r+b");
+    if (fp == NULL) {
+        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+       printf("Não é possível continuar o programa...\n");
+        exit(1);
+    }
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("///|                                                                 |///\n");
+    printf("///|                    *** SIG-Finance ***                          |///\n");
+    printf("///|                                                                 |///\n");
+    printf("///|_________________________________________________________________|///\n");
+    printf("///                    **   Excluir  Despesa **                       ///\n");
+    printf("///                                                                   ///\n");
+    printf("///                                                                   ///\n");
+    printf("///  Digite a despesa a ser excluida:                                 ///\n");
+    scanf(" %14[^\n]",alterar);
+    rec = (Despesa*) malloc(sizeof(Despesa));
+    achou = 0;
+    while((!achou)&&(fread(rec,sizeof(Despesa), 1,fp))){
+       if((strcmp(des->tipo, alterar) == 0) && (des->status == '1')){
+         achou = 1;
+       }
+    }
+  
+    if (achou) {
+        exibeDespesa(des);
+        getchar();
+        printf("Deseja realmente apagar esta despesa (s/n)? ");
+        scanf("%c", &resp);
+        if (resp == 's' || resp == 'S') {
+            des->status = '0';
+            fseek(fp, (-1)*sizeof(Despesa), SEEK_CUR);
+            fwrite(rec, sizeof(Despesa), 1, fp);
+            printf("\nDespesa excluída com sucesso!!!\n");
+        } else {
+            printf("\n Os dados não foram alterados\n");
+        }
+     } else {
+         printf("Esta despesa %s não foi encontrada..\n", alterar);
+    }
+    free(des);
+    fclose(fp);
+}
     printf("Tecle ENTER pra continuar..");
     getchar();
 }
 
 void incluirProduto(void){
-     Produto* pro;
+    Produto* pro;
     pro = (Produto*) malloc(sizeof(Produto));
     printf("\n");
     printf("/////////////////////////////////////////////////////////////////////////\n");
@@ -319,8 +516,55 @@ void alterarProduto(void){
 }
 
 void excluirProduto(void){
-    printf("\n///   **   Excluir Produto  **   ///\n\n");
-    printf(" Em desenvolvimento...\n\n");
+    FILE* fp;
+    Produto* pro;
+    int achou;
+    char resp;
+    char alterar[26];
+    fp = fopen("produto.dat", "r+b");
+    if (fp == NULL) {
+       printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+       printf("Não é possível continuar o programa...\n");
+        exit(1);
+    }
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////\n");
+    printf("///|                                                                 |///\n");
+    printf("///|                    *** SIG-Finance ***                          |///\n");
+    printf("///|                                                                 |///\n");
+    printf("///|_________________________________________________________________|///\n");
+    printf("///                    **   Excluir  Produto **                       ///\n");
+    printf("///                                                                   ///\n");
+    printf("///                                                                   ///\n");
+    printf("///  Digite o produto a ser excluido:                                 ///\n");
+    scanf(" %14[^\n]",alterar);
+    pro = (Produto*) malloc(sizeof(Produto));
+    achou = 0;
+    while((!achou)&&(fread(pro,sizeof(Produto), 1,fp))){
+       if((strcmp(pro->tipo, alterar) == 0) && (pro->status == '1')){
+         achou = 1;
+       }
+    }
+  
+    if (achou) {
+        exibeProduto(pro);
+        getchar();
+        printf("Deseja realmente apagar este produto (s/n)? ");
+        scanf("%c", &resp);
+        if (resp == 's' || resp == 'S') {
+            pro->status = '0';
+            fseek(fp, (-1)*sizeof(Produto), SEEK_CUR);
+            fwrite(pro, sizeof(Produto), 1, fp);
+            printf("\nProduto excluído com sucesso!!!\n");
+        } else {
+            printf("\n Os dados não foram alterados\n");
+        }
+     } else {
+         printf("Este produto %s não foi encontrada..\n", alterar);
+    }
+    free(pro);
+    fclose(fp);
+}
     printf("Tecle ENTER pra continuar..");
     getchar();
 
